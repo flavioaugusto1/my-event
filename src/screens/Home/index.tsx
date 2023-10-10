750309435b1bc5f386e7da0fefc94c49b2910f83
Flávio <f.augustosdn.dev@gmail.com>
2023-10-09 23:10:10 -0300

    implementado o input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
-import { Text, View } from "react-native"
+import { Text, View, TextInput } from "react-native"
 import { styles } from "./styles"
 
-export default function Home() {
+export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -10,6 +10,12 @@ export default function Home() {
       <Text style={styles.eventDate}>
         Quarta, 29 de novembro de 2023.
       </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,16 +4,24 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#131016',
-    padding: 12
+    padding: 16
   },
   eventName: {
     fontWeight: 'bold',
     fontSize: 36,
     marginTop: 48,
-    color: '#FDFCFE'
+    color: '#FFF'
   },
   eventDate: {
     fontSize: 16,
     color: '#6B6B6B'
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    height: 56,
+    borderRadius: 5,
+    color: '#FFF',
+    padding: 16,
+    fontSize: 16
   }
 })
